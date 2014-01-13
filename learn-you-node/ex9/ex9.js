1f9bf4a1caa75e787b270c0dbfe2353cb2a77017
posbo <paul.osborne.uk@gmail.com>
2014-01-13 16:03:27 +0000

    ex9

--- learn-you-node/ex9/ex9.js
+++ learn-you-node/ex9/ex9.js
@@ -1,8 +1,27 @@
 var http    = require('http'),
-    async   = require('async');
+    bl      = require('bl'),
+    results = [],
+    count   = 0;
 
-process.argv.forEach(function(url) {
-    http.get(url, function () {
-        console.log(url);
+function printResults () {
+    for(var i = 0; i < 3; i++)
+        console.log(results[i]);
+}
+
+function httpGet (index) {
+    http.get(process.argv[2 + index], function (response) {
+        response.pipe(bl(function (err, data) {
+            if (err)
+                return console.error(data);
+            
+            results[index] = data.toString();
+            count++;
+
+            if (count == 3)
+                printResults();
+        }));
     });
-});
+}
+
+for (var i = 0; i < 3; i++)
+    httpGet(i);
